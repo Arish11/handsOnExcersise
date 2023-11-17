e7813895bbbed18c0db9e936e3b011e556b2aae1
arish11 <abhishekarya.8596@gmail.com>
2023-11-17 19:49:06 +0530

    it works on online compiler but is failing in IDE

--- HandsonExcercise/src/collections/LinkedList.java
+++ HandsonExcercise/src/collections/LinkedList.java
@@ -5,36 +5,43 @@ import java.util.Scanner;
 
 public class LinkedList {
 
-	public static void main(String[] args) {
-		LinkedList linkedList = new LinkedList();
-		linkedList.add();
-	
-		int choice;
-		Scanner sc = new Scanner(System.in);
-		System.out.println("What do want to do choose ? \n1)add\n2)remove\n3)view");
-		choice = sc.nextInt();
-		switch (choice) {
-		case 1: {
-			int count = 0;
-			System.out.println("How many element you want to add?");
-			for(int i=0; i<count;i++) {
-				
+	 public static void main(String[] args) {
+			LinkedList linkedList = new LinkedList();
+			
+			int choice;
+			Scanner sc = new Scanner(System.in);
+			System.out.println("What do want to do choose ? \n1)add\n2)remove\n3)view");
+			choice = sc.nextInt();
+			switch (choice) {
+			case 1: {
+				int count = 0;
+				System.out.println("How many element you want to add?");
+				count = sc.nextInt();
+				for(int i=0; i<count;i++) {
+				    int s;
+					System.out.println("Enter the "+i+" element");
+					s = sc.nextInt();
+					linkedList.add(s);
+				}
+				break;
 			}
-			break;
-		}
-		
-		case 2: {
-			System.out.println("Removing");
-			break;
-		}
-		
-		case 3: {
-			System.out.println("Viewing");
-			break;
-		}
-		default:
-			throw new IllegalArgumentException("Unexpected value: " + choice);
-		}
-	}
+			
+			case 2: {
+			    int index;
+				System.out.println("Enter the index to remove");
+				index = sc.nextInt();
+				linkedList.remove(index);
+				break;
+			}
+			
+			case 3: {
+				System.out.println("Viewing");
+				break;
+			}
+			default:
+				throw new IllegalArgumentException("Unexpected value: " + choice);
+			}
+		    System.out.println("The linked list now is "+linkedList);
+	    }
 
 }
