95023b04b18ba1e73b73eb46250d577cdb1d254d
arish11 <abhishekarya.8596@gmail.com>
2023-11-22 14:00:32 +0530

    added more questions I can practice

---
+++ HandsonExcercise/src/logical/LargestElement.java
@@ -0,0 +1,10 @@
+package logical;
+
+public class LargestElement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ HandsonExcercise/src/stream/Comments.java
@@ -0,0 +1,25 @@
+package stream;
+
+public class Comments {
+/*	
+	Question 31: Filtering and Joining
+
+	Given a list of strings, filter out the strings that contain the letter 'a', convert the remaining strings to uppercase, and join them into a single comma-separated string.
+
+	Question 32: Mapping and Averaging
+
+	Suppose you have a list of integers. Use the Stream API to square each integer, then calculate the average of the squared values.
+
+	Question 33: Filtering and Distinct
+
+	Given a list of integers, filter out the even numbers, and then find the distinct odd numbers. Collect the results into a new list.
+
+	Question 34: Partitioning and Counting
+
+	Given a list of strings, partition them into two groups: one group containing strings of length less than 5 and another group containing strings of length 5 or more. Count the number of strings in each group.
+
+	Question 35: Mapping and Reducing
+
+	Imagine you have a list of doubles representing product prices. Use the Stream API to increase each price by 10%, and then calculate the total discounted amount
+*/
+}
