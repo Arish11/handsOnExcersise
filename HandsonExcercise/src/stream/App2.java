d550eae1dd3b09ea758315bcd7179d8517d85464
arish11 <abhishekarya.8596@gmail.com>
2023-11-22 12:41:36 +0530

    practising stream api

---
+++ HandsonExcercise/src/stream/App2.java
@@ -0,0 +1,22 @@
+package stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class App2 {
+
+	public static void main(String[] args) {
+		List<Integer> list = new ArrayList<Integer>();		
+		list.add(10);
+		list.add(1);
+		list.add(19);
+		list.add(3);
+		list.add(18);
+		list.add(11);
+		list.add(4);
+		List<Integer> sqList = list.stream().filter(x -> x%2 == 0).map( x -> x*x).collect(Collectors.toList());
+		System.out.println("Squared list is "+sqList);
+	}
+
+}
