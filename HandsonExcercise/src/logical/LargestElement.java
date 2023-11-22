1da09a08d1be5e236e705b88b077d9d7442c365d
arish11 <abhishekarya.8596@gmail.com>
2023-11-22 14:11:18 +0530

    largest in array

--- HandsonExcercise/src/logical/LargestElement.java
+++ HandsonExcercise/src/logical/LargestElement.java
@@ -3,8 +3,15 @@ package logical;
 public class LargestElement {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int [] arr = {1,2,512,23,11,43,64,27,8,4};
+		int len = arr.length;
+		int largest = 0;
+		for(int i=0;i<len;i++) {
+			if(arr[i]>largest) {
+				largest = arr[i];
+			}
+		}
+		System.out.println("The largest element in array is "+largest);
 	}
 
 }
